d6fe831ea33a10d96e771065d2f40b1ca3a46565
Sofia <sofia@gimholt.se>
2025-10-01 16:42:21 +0200

    Fix not reaching the API

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,21 @@ import App from "./App";
 import HomeRoute from "./routes/HomeRoute";
 import DocRoute from "./routes/DocRoute";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { index: true, element: <HomeRoute /> },
+        { path: ":id", element: <DocRoute /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <HomeRoute /> },
-      { path: ":id", element: <DocRoute /> },
-    ],
-  },
-]);
+    basename: "/DV1677-ssr-frontend",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
